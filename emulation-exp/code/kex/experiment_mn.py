f254ce165d071e55cf9787cd4dd1af0ff3b4953d
lin-1214 <b10901059@g.ntu.edu.tw>
2024-11-27 03:21:10 +0800

    add debug message

--- emulation-exp/code/kex/experiment_mn.py
+++ emulation-exp/code/kex/experiment_mn.py
@@ -49,14 +49,12 @@ def time_handshake(kex_alg, measurements):
     """Run handshake timing test from a Mininet host."""
     command = f"sh ./s_timer.o {kex_alg} {measurements}"
     # print(f"[DEBUG] Executing command on client: {command}")
-    try:
-        result = client.cmd(command)
-        # print(f"[DEBUG] Command output: {result}")
-        print(f"[DEBUG] Result: {result}")
-        return [float(i) for i in result.strip().split(",")]
-    except Exception as e:
-        print(f"[ERROR] Error executing command on client: {e}")
-        return []
+    
+    result = client.cmd(command)
+    # print(f"[DEBUG] Command output: {result}")
+    print(f"[DEBUG] Result: {result}")
+    return [float(i) for i in result.strip().split(",")]
+    
 
 
 
