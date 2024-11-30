b0b3330279b7d90d3eff031322bcfb4f225da141
lin-1214 <b10901059@g.ntu.edu.tw>
2024-11-30 12:39:15 +0800

    modify setup file

--- emulation-exp/code/sig/setup.sh
+++ emulation-exp/code/sig/setup.sh
@@ -36,12 +36,11 @@ ${OPENSSL} req -new -newkey ec:prime256v1.pem -keyout ${NGINX_CONF_DIR}/ecdsap25
 ${OPENSSL} x509 -req -in ${NGINX_CONF_DIR}/ecdsap256_server.csr -out ${NGINX_CONF_DIR}/ecdsap256_server.crt -CA ${NGINX_CONF_DIR}/ecdsap256_CA.crt -CAkey ${NGINX_CONF_DIR}/ecdsap256_CA.key -CAcreateserial -days 365
 
 ## Generate all other certs
-SIG = dilithium2
-do
-    # generate CA key and cert
-    ${OPENSSL} req -x509 -new -newkey ${SIG} -keyout ${NGINX_CONF_DIR}/${SIG}_CA.key -out ${NGINX_CONF_DIR}/${SIG}_CA.crt -nodes -subj "/CN=OQS test ${SIG} CA" -days 365 -config ${OPENSSL_CNF}
-    # generate server CSR
-    ${OPENSSL} req -new -newkey ${SIG} -keyout ${NGINX_CONF_DIR}/${SIG}_server.key -out ${NGINX_CONF_DIR}/${SIG}_server.csr -nodes -subj "/CN=oqstest ${SIG}" -config ${OPENSSL_CNF}
-    # generate server cert
-    ${OPENSSL} x509 -req -in ${NGINX_CONF_DIR}/${SIG}_server.csr -out ${NGINX_CONF_DIR}/${SIG}_server.crt -CA ${NGINX_CONF_DIR}/${SIG}_CA.crt -CAkey ${NGINX_CONF_DIR}/${SIG}_CA.key -CAcreateserial -days 365
-done
+SIG="dilithium2"
+
+# generate CA key and cert
+${OPENSSL} req -x509 -new -newkey ${SIG} -keyout ${NGINX_CONF_DIR}/${SIG}_CA.key -out ${NGINX_CONF_DIR}/${SIG}_CA.crt -nodes -subj "/CN=OQS test ${SIG} CA" -days 365 -config ${OPENSSL_CNF}
+# generate server CSR
+${OPENSSL} req -new -newkey ${SIG} -keyout ${NGINX_CONF_DIR}/${SIG}_server.key -out ${NGINX_CONF_DIR}/${SIG}_server.csr -nodes -subj "/CN=oqstest ${SIG}" -config ${OPENSSL_CNF}
+# generate server cert
+${OPENSSL} x509 -req -in ${NGINX_CONF_DIR}/${SIG}_server.csr -out ${NGINX_CONF_DIR}/${SIG}_server.crt -CA ${NGINX_CONF_DIR}/${SIG}_CA.crt -CAkey ${NGINX_CONF_DIR}/${SIG}_CA.key -CAcreateserial -days 365
