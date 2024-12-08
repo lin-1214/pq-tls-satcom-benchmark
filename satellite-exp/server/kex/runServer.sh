e5c8acb99fea2ed8d03147e4dd816ee5cd6ef722
lin-1214 <b10901059@g.ntu.edu.tw>
2024-12-09 02:35:47 +0800

    setup server

---
+++ satellite-exp/server/kex/runServer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# set -ex
+
+echo "Running key exchange experiment... ⚡"
+
+ROOT="$(dirname $(pwd))"
+
+NGINX_APP=${ROOT}/tmp/nginx/sbin/nginx
+NGINX_CONF_DIR=${ROOT}/tmp/nginx/conf
+
+sudo python3 ${ROOT}/kex/server.py ${NGINX_APP} ${NGINX_CONF_DIR}/nginx.conf
