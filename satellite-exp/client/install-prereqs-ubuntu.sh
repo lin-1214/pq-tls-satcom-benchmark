08ded01e2bc41b47d37bd92863c8d69623a2e759
lin-1214 <b10901059@g.ntu.edu.tw>
2024-12-09 12:17:46 +0800

    modify scripts

--- satellite-exp/client/install-prereqs-ubuntu.sh
+++ satellite-exp/client/install-prereqs-ubuntu.sh
@@ -27,6 +27,7 @@ ROOT=$(pwd)
 wget https://cmake.org/files/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.${CMAKE_BUILD}-Linux-x86_64.sh
 git clone --single-branch --branch pq-tls-experiment https://github.com/xvzcf/liboqs
 git clone --single-branch --branch pq-tls-experiment https://github.com/xvzcf/openssl
+wget nginx.org/download/nginx-${NGINX_VERSION}.tar.gz && tar -zxvf nginx-${NGINX_VERSION}.tar.gz
 
 # Install the latest CMake
 mkdir cmake
