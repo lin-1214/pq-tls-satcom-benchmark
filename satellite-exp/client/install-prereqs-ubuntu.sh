c77ee14b5a539a8e19c77f2e8bd1fda104edaadb
lin-1214 <b10901059@g.ntu.edu.tw>
2024-12-09 12:13:02 +0800

    modify scripts

--- satellite-exp/client/install-prereqs-ubuntu.sh
+++ satellite-exp/client/install-prereqs-ubuntu.sh
@@ -37,3 +37,16 @@ cd liboqs
 mkdir build && cd build
 ${ROOT}/cmake/bin/cmake -GNinja -DCMAKE_INSTALL_PREFIX=${ROOT}/openssl/oqs ..
 ninja && ninja install
+
+# build nginx (which builds OQS-OpenSSL)
+cd ${ROOT}
+cd nginx-${NGINX_VERSION}
+./configure --prefix=${ROOT}/nginx \
+                --with-debug \
+                --with-http_ssl_module --with-openssl=${ROOT}/openssl \
+                --without-http_gzip_module \
+                --with-cc-opt="-I ${ROOT}/openssl/oqs/include" \
+                --with-ld-opt="-L ${ROOT}/openssl/oqs/lib";
+sed -i 's/libcrypto.a/libcrypto.a -loqs/g' objs/Makefile;
+sed -i 's/EVP_MD_CTX_create/EVP_MD_CTX_new/g; s/EVP_MD_CTX_destroy/EVP_MD_CTX_free/g' src/event/ngx_event_openssl.c;
+make && make install;
